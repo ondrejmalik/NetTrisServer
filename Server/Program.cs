f8fee675304a70a1d47f5e35d3390dc75f0f9e49
o.m777@email.cz <malik@spsejecna.cz>
2024-04-19 03:15:22 +0200

    added waiting for second player

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,8 +2,10 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+
 Console.WriteLine("Server is Running..");
 HashSet<(int index, UdpClient client, IPEndPoint ipEndPoint)> clients = new();
+object _EnumerationLock = new();
 int index = 0;
 while (true)
 {
@@ -18,7 +20,7 @@ while (true)
     client.Close();
     (int index, UdpClient client, IPEndPoint ipEndPoint) newClient = new();
     Console.WriteLine($"New Client Connected - {ipEndPoint.Address.ToString()}");
-    Console.WriteLine("Current Clients: " + clients.Count);
+    Console.WriteLine("Current Clients: " + clients.Count); // TODO: need to remove disconnected clients
     newClient.index = index;
     index++;
     newClient.client = new UdpClient();
@@ -26,38 +28,69 @@ while (true)
     string message = "OK";
     data = Encoding.ASCII.GetBytes(message);
     newClient.client.Send(data, data.Length, newClient.ipEndPoint);
-    clients.Add(newClient);
-    if (index % 2 == 1)
+    lock (_EnumerationLock)
     {
-        Thread thread = new Thread(() => SendLoop(index - 1, index));
-        thread.Start();
+        clients.Add(newClient);
+        if (index % 2 == 1)
+        {
+            Thread thread = new Thread(() => SendLoop(index - 1, index));
+            thread.Start();
+        }
     }
 }
 
 void SendLoop(int player1Index, int player2Index)
 {
+    (int index, UdpClient client, IPEndPoint ipEndPoint) valueTuple =
+        clients.Where(item => item.index == player1Index).First();
+    IEnumerable<(int index, UdpClient client, IPEndPoint ipEndPoint)> valueTuple2;
+    Stopwatch s = new Stopwatch();
+    byte[] recieved1;
+    byte[] result2;
     while (true)
     {
-        Stopwatch s = new Stopwatch();
-        s.Start();
-        byte[] recieved1;
-
-        var valueTuple = clients.Where(item => item.index == player1Index).First();
-        recieved1 = valueTuple.client.Receive(ref valueTuple.ipEndPoint);
-        //Console.WriteLine(Encoding.ASCII.GetString(recieved1)); //-- this is the message from the client1
-
-        var valueTuple2 = clients.Where(item => item.index == player2Index);
-        byte[] result2 = new byte[0];
-        if (valueTuple2.Count() > 0)
+        lock (_EnumerationLock)
         {
+            // wait for the second player to connect
+            valueTuple2 = clients.Where(item => item.index == player2Index);
+            if (valueTuple2.Count() > 0)
+            {
+                Console.WriteLine("Both Players Connected");
+                var client2 = valueTuple2.First();
+                byte[] readyMessage = Encoding.ASCII.GetBytes("GameReady");
+                // TODO: add makin sure the readyMessages are recieved 
+                client2.client.Send(readyMessage, readyMessage.Length, client2.ipEndPoint);
+                valueTuple.client.Send(readyMessage, readyMessage.Length, valueTuple.ipEndPoint);
+                Console.WriteLine("Game Ready Sent");
+                break;
+            }
+        }
+    }
+
+    try
+    {
+        while (true)
+        {
+            s.Start();
+
+            recieved1 = valueTuple.client.Receive(ref valueTuple.ipEndPoint);
+            //Console.WriteLine(Encoding.ASCII.GetString(recieved1)); //-- this is the message from the client1
+
+            valueTuple2 = clients.Where(item => item.index == player2Index);
+
+
             var client2 = valueTuple2.First();
             result2 = client2.client.Receive(ref client2.ipEndPoint);
             //Console.WriteLine(Encoding.ASCII.GetString(result2)); //-- this is the message from the client2
             client2.client.Send(recieved1, recieved1.Length, client2.ipEndPoint);
-        }
 
-        valueTuple.client.Send(result2, result2.Length, valueTuple.ipEndPoint);
-        s.Stop();
-        Console.WriteLine(s.ElapsedMilliseconds);
+            valueTuple.client.Send(result2, result2.Length, valueTuple.ipEndPoint);
+            s.Stop();
+            Console.WriteLine(s.ElapsedMilliseconds);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Clients {player1Index} and {player2Index} Disconnected");
     }
 }
